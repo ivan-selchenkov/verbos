b2051df74250092ee303037c11c97776ba584f2b
Ivan V Selchenkov <home@vashik.info>
2010-12-11 20:09:11 +0300

    config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Verbos::Application.config.secret_token = 'c9e9de035f67e778feea5d6913ad6c40592d357c493a3ca3869b40efe8633a4641f3d5b77481b52a7d0bc5a1658978afb9682054cca23c49538d3b488b6061c0'
