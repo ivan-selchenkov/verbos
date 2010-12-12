551ca6e4c16a686eb7cbc670b99f89cb88896c60
Ivan V Selchenkov <home@vashik.info>
2010-12-12 14:21:02 +0300

    parsed OK

--- lib/load_verbos.rb
+++ lib/load_verbos.rb
@@ -12,26 +12,84 @@ doc = Nokogiri::HTML open(
 )
 
 i_modo = 0
+i_tiempo = 0
+
+modos = ['', '', 'Indicativo', 'Subjuntivo', 'Condicional', 'Imperativo']
+
+tiempos = {
+  '' => '',
+  'Indicativo' => [
+    'Presente',
+    'Pretérito perfecto',
+    'Pretérito imperfecto',
+    'Pretérito pluscuamperfecto',
+    'Pretérito perfecto simple',
+    'Pretérito anterior',
+    'Futuro',
+    'Futuro perfecto'
+  ],
+  'Subjuntivo' => [
+    'Presente',
+    'Pretérito perfecto',
+    'Pretérito imperfecto',
+    'Pretérito pluscuamperfecto',
+    'Futuro',
+    'Futuro perfecto'
+  ],
+  'Condicional' => [
+    'Condicional',
+    '',
+    'Condicional compuesto'
+  ],
+  'Imperativo' => [
+    'Afirmativo',
+    'Negativo'
+  ]
+}
+
+negativo = ['tú', 'él', 'nosotros', 'vosotros', 'ellos']
 
 formas = ['yo', 'tú', 'él', 'nosotros', 'vosotros', 'ellos']
 
+
+
 doc.css('table.verbtable td.verbtable').each { |modo|
   i_modo += 1
-  next if i_modo < 3
+  next if i_modo < 3 # skip
+  next if modos[i_modo - 1].nil?
+
+  i_tiempo = 0
+
   modo.css('table td').each { |tiempo|
+    next if tiempo.text.strip.empty?
+
+    next if tiempos[modos[i_modo - 1]][i_tiempo].empty?
+
     i_verbo = 1
+
+    p modos[i_modo - 1] + "*****************" + tiempos[modos[i_modo - 1]][i_tiempo]
+
     forma = ""
     tiempo.css('span').each { |verbo|
       next if verbo.text.strip.empty?
 
+      break if i_verbo > 12
+
       if i_verbo % 2 == 0
         v = verbo.text
+
+        if modos[i_modo - 1] == 'Imperativo'
+          forma = negativo[ (i_verbo / 2) -1 ]
+        end
+
         p forma + " - " + v
+
       else
         forma = verbo.text
       end
       i_verbo += 1
     }
+    i_tiempo += 1
   }
 
 }
